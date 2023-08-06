b6583b4c0ab813dfd66e992da5b545f2fa8c4b91
Mitsu325 <patricia.mashiba@gmail.com>
2023-08-06 20:58:02 -0300

    chore: add --allow-root

--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Baixar o wordpress
-RUN wp core download
+RUN wp core download --allow-root
 
 # Criar arquivo de configuração wp-config.php com dados do banco
 wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER \
-	--dbpass=$DATABASE_PASSWORD --dbhost=$DATABASE_HOST
+	--dbpass=$DATABASE_PASSWORD --dbhost=$DATABASE_HOST --allow-root
 
 # Instalar o wordpress
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE \
 	--admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS \
-	--admin_email=$WP_ADMIN_EMAIL
+	--admin_email=$WP_ADMIN_EMAIL --allow-root
 
 # Criar usuário
-wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS
+wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
 
 php-fpm81 -F
