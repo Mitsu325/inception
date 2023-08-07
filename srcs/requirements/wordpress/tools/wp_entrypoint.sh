9302903ca2fa5a3178d87ccba5dfefd603b1ef68
Mitsu325 <patricia.mashiba@gmail.com>
2023-08-06 21:22:40 -0300

    chore: config tools

--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
 # Baixar o wordpress
-wp core download --allow-root
+wp core download --allow-root --path=/data/wordpress
 
 # Criar arquivo de configuração wp-config.php com dados do banco
-wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER \
-	--dbpass=$DATABASE_PASSWORD --dbhost=$DATABASE_HOST --allow-root
+wp config create --allow-root --path=/data/wordpress \
+	--dbname=$DATABASE_NAME \
+	--dbuser=$DATABASE_USER \
+	--dbpass=$DATABASE_PASSWORD \
+	--dbhost=$DATABASE_HOST
 
 # Instalar o wordpress
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE \
-	--admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS \
-	--admin_email=$WP_ADMIN_EMAIL --allow-root
+wp core install --allow-root --path=/data/wordpress \
+	--url=$DOMAIN_NAME \
+	--title=$WP_TITLE \
+	--admin_user=$WP_ADMIN_NAME \
+	--admin_password=$WP_ADMIN_PASS \
+	--admin_email=$WP_ADMIN_EMAIL
 
 # Criar usuário
-wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
+wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL \
+	--role=author \
+	--user_pass=$WP_USER_PASS
 
 php-fpm81 -F
