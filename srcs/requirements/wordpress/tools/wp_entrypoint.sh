3dc43668e6230babaeb635234dd553618927d7f8
Mitsu325 <patricia.mashiba@gmail.com>
2023-08-06 21:55:59 -0300

    chore: wordpress

--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# Instalar o wordpress
-wp core install --allow-root --path=/data/wordpress \
-	--url=$DOMAIN_NAME \
-	--title=$WP_TITLE \
-	--admin_user=$WP_ADMIN_NAME \
-	--admin_password=$WP_ADMIN_PASS \
-	--admin_email=$WP_ADMIN_EMAIL
 
-# Criar usuário
-wp user create $WP_USER_NAME $WP_USER_EMAIL \
-	--role=author \
-	--user_pass=$WP_USER_PASS --allow-root
 
 php-fpm81 -F
