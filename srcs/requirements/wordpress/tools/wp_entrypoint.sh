c349fad189b21d1d989c8c3d1d2407b24160fd7f
Mitsu325 <patricia.mashiba@gmail.com>
2023-08-06 21:24:59 -0300

    chore: config wordpress

--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Baixar o wordpress
-wp core download --allow-root --path=/data/wordpress
-
 # Criar arquivo de configuração wp-config.php com dados do banco
 wp config create --allow-root --path=/data/wordpress \
 	--dbname=$DATABASE_NAME \
@@ -19,8 +16,8 @@ wp core install --allow-root --path=/data/wordpress \
 	--admin_email=$WP_ADMIN_EMAIL
 
 # Criar usuário
-wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL \
+wp user create $WP_USER_NAME $WP_USER_EMAIL \
 	--role=author \
-	--user_pass=$WP_USER_PASS
+	--user_pass=$WP_USER_PASS --allow-root
 
 php-fpm81 -F
